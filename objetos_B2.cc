cd7788fc07e657a2b4e863bfdc115d2573518102
Sergio Zapata <sergiozap1@gmail.com>
2023-10-02 15:45:29 +0200

    initial commit

---
+++ objetos_B2.cc
@@ -0,0 +1,254 @@
+//**************************************************************************
+// Práctica 2 
+//**************************************************************************
+
+#include "objetos_B2.h"
+#include "file_ply_stl.hpp"
+
+
+//*************************************************************************
+// _puntos3D
+//*************************************************************************
+
+_puntos3D::_puntos3D()
+{
+}
+
+//*************************************************************************
+// dibujar puntos
+//*************************************************************************
+
+void _puntos3D::draw_puntos(float r, float g, float b, int grosor)
+{
+int i;
+glPointSize(grosor);
+glColor3f(r,g,b);
+glBegin(GL_POINTS);
+for (i=0;i<vertices.size();i++){
+	glVertex3fv((GLfloat *) &vertices[i]);
+	}
+glEnd();
+}
+
+
+//*************************************************************************
+// _triangulos3D
+//*************************************************************************
+
+_triangulos3D::_triangulos3D()
+{
+}
+
+
+//*************************************************************************
+// dibujar en modo arista
+//*************************************************************************
+
+void _triangulos3D::draw_aristas(float r, float g, float b, int grosor)
+{
+int i;
+glPolygonMode(GL_FRONT_AND_BACK,GL_LINE);
+glLineWidth(grosor);
+glColor3f(r,g,b);
+glBegin(GL_TRIANGLES);
+for (i=0;i<caras.size();i++){
+	glVertex3fv((GLfloat *) &vertices[caras[i]._0]);
+	glVertex3fv((GLfloat *) &vertices[caras[i]._1]);
+	glVertex3fv((GLfloat *) &vertices[caras[i]._2]);
+	}
+glEnd();
+}
+
+//*************************************************************************
+// dibujar en modo sólido con un único color
+//*************************************************************************
+
+void _triangulos3D::draw_solido(float r, float g, float b)
+{
+
+
+
+}
+
+//*************************************************************************
+// dibujar en modo sólido con colores diferentes para cada cara
+//*************************************************************************
+
+void _triangulos3D::draw_solido_colores()
+{
+
+}
+
+//*************************************************************************
+// dibujar con distintos modos
+//*************************************************************************
+
+void _triangulos3D::draw(_modo modo, float r, float g, float b, float grosor)
+{
+switch (modo){
+	case POINTS:draw_puntos(r, g, b, grosor);break;
+	case EDGES:draw_aristas(r, g, b, grosor);break;
+	case SOLID:draw_solido(r, g, b);break;
+	case SOLID_COLORS:draw_solido_colores();break;
+	}
+}
+
+//*************************************************************************
+// clase cubo
+//*************************************************************************
+
+_cubo::_cubo(float tam)
+{
+// vertices
+
+
+// triangulos
+
+}
+
+
+//*************************************************************************
+// clase piramide
+//*************************************************************************
+
+_piramide::_piramide(float tam, float al)
+{
+
+//vertices 
+vertices.resize(5); 
+vertices[0].x=-tam;vertices[0].y=0;vertices[0].z=tam;
+vertices[1].x=tam;vertices[1].y=0;vertices[1].z=tam;
+vertices[2].x=tam;vertices[2].y=0;vertices[2].z=-tam;
+vertices[3].x=-tam;vertices[3].y=0;vertices[3].z=-tam;
+vertices[4].x=0;vertices[4].y=al;vertices[4].z=0;
+
+caras.resize(6);
+caras[0]._0=0;caras[0]._1=1;caras[0]._2=4;
+caras[1]._0=1;caras[1]._1=2;caras[1]._2=4;
+caras[2]._0=2;caras[2]._1=3;caras[2]._2=4;
+caras[3]._0=3;caras[3]._1=0;caras[3]._2=4;
+caras[4]._0=3;caras[4]._1=1;caras[4]._2=0;
+caras[5]._0=3;caras[5]._1=2;caras[5]._2=1;
+}
+
+//*************************************************************************
+// clase objeto ply
+//*************************************************************************
+
+
+_objeto_ply::_objeto_ply() 
+{
+   // leer lista de coordenadas de vértices y lista de indices de vértices
+ 
+}
+
+
+
+void _objeto_ply::parametros(char *archivo)
+{
+int n_ver,n_car;
+
+vector<float> ver_ply ;
+vector<int>   car_ply ;
+ 
+_file_ply::read(archivo, ver_ply, car_ply );
+
+n_ver=ver_ply.size()/3;
+n_car=car_ply.size()/3;
+
+printf("Number of vertices=%d\nNumber of faces=%d\n", n_ver, n_car);
+
+vertices.resize(n_ver);
+caras.resize(n_car);
+
+}
+
+
+//************************************************************************
+// objeto por revolucion
+//************************************************************************
+
+_rotacion::_rotacion()
+{
+
+}
+
+
+void _rotacion::parametros(vector<_vertex3f> perfil, int num)
+{
+int i,j;
+_vertex3f vertice_aux;
+_vertex3i cara_aux;
+int num_aux;
+
+// tratamiento de los vértice
+
+num_aux=perfil.size();
+vertices.resize(num_aux*num);
+for (j=0;j<num;j++)
+  {for (i=0;i<num_aux;i++)
+     {
+      vertice_aux.x=perfil[i].x*cos(2.0*M_PI*j/(1.0*num))+
+                    perfil[i].z*sin(2.0*M_PI*j/(1.0*num));
+      vertice_aux.z=-perfil[i].x*sin(2.0*M_PI*j/(1.0*num))+
+                    perfil[i].z*cos(2.0*M_PI*j/(1.0*num));
+      vertice_aux.y=perfil[i].y;
+      vertices[i+j*num_aux]=vertice_aux;
+     }
+  }
+
+// tratamiento de las caras 
+
+     
+// tapa inferior
+
+ 
+// tapa superior
+ 
+}
+
+
+
+//************************************************************************
+// objeto por extrusión
+//************************************************************************
+
+
+_extrusion::_extrusion(vector<_vertex3f> poligono, float x, float y, float z)
+{
+int i;
+_vertex3f vertice_aux;
+_vertex3i cara_aux;
+int num_aux;
+
+// tratamiento de los vértice
+
+num_aux=poligono.size();
+vertices.resize(num_aux*2);
+for (i=0;i<num_aux;i++)
+    {
+      vertices[2*i]=poligono[i];
+      vertices[2*i+1].x=poligono[i].x+x;
+      vertices[2*i+1].y=poligono[i].y+y;
+      vertices[2*i+1].z=poligono[i].z+z;
+    }
+    
+// tratamiento de las caras 
+
+caras.resize(num_aux*2);
+int c=0;
+for (i=0;i<num_aux;i++)         
+  {
+   caras[c]._0=i*2;
+   caras[c]._1=(i*2+2)%(num_aux*2);
+   caras[c]._2=i*2+1;    
+   c=c+1;
+   caras[c]._0=(i*2+2)%(num_aux*2);
+   caras[c]._1=(i*2+2)%(num_aux*2)+1;
+   caras[c]._2=i*2+1;    
+   c=c+1;    
+      
+   }   
+}
+
+
---
+++ objetos_B2.h
@@ -0,0 +1,104 @@
+//**************************************************************************
+// Práctica 2 
+//**************************************************************************
+
+#include <vector>
+#include <GL/gl.h>
+#include "vertex.h"
+#include <stdlib.h>
+
+
+const float AXIS_SIZE=5000;
+typedef enum{POINTS,EDGES,SOLID,SOLID_COLORS} _modo;
+
+//*************************************************************************
+// clase punto
+//*************************************************************************
+
+class _puntos3D
+{
+public:
+
+  
+	_puntos3D();
+void 	draw_puntos(float r, float g, float b, int grosor);
+
+vector<_vertex3f> vertices;
+};
+
+//*************************************************************************
+// clase triángulo
+//*************************************************************************
+
+class _triangulos3D: public _puntos3D
+{
+public:
+
+	_triangulos3D();
+void 	draw_aristas(float r, float g, float b, int grosor);
+void    draw_solido(float r, float g, float b);
+void 	draw_solido_colores();
+void 	draw(_modo modo, float r, float g, float b, float grosor);
+
+vector<_vertex3i> caras;
+};
+
+
+//*************************************************************************
+// clase cubo
+//*************************************************************************
+
+class _cubo: public _triangulos3D
+{
+public:
+
+	_cubo(float tam=0.5);
+};
+
+
+//*************************************************************************
+// clase piramide
+//*************************************************************************
+
+class _piramide: public _triangulos3D
+{
+public:
+
+	_piramide(float tam=0.5, float al=1.0);
+};
+
+//*************************************************************************
+// clase objeto ply
+//*************************************************************************
+
+class _objeto_ply: public _triangulos3D
+{
+public:
+   _objeto_ply();
+
+void  parametros(char *archivo);
+};
+
+//************************************************************************
+// objeto por revolución
+//************************************************************************
+
+class _rotacion: public _triangulos3D
+{
+public:
+       _rotacion();
+       
+void  parametros(vector<_vertex3f> perfil, int num);
+};
+
+
+//************************************************************************
+// objeto por extrusión
+//************************************************************************
+
+class _extrusion: public _triangulos3D
+{
+public:
+       _extrusion(vector<_vertex3f> poligono, float x, float y, float z);
+};
+
